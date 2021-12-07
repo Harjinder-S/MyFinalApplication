d30661bc38679027b2117a2d8e4c519d81eb3a33
Harjinder_Singh <sing1477@algonquinlive.com>
2021-12-07 12:58:53 -0500

    covid 19 opening

--- app/src/main/java/algonquin/cst2335/myfinalapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myfinalapplication/MainActivity.java
@@ -2,13 +2,26 @@ package algonquin.cst2335.myfinalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button covid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        covid = findViewById(R.id.covid);
+        // calling click listener on button to go to second activity
+        covid.setOnClickListener(click ->{
+        Intent nextPage = new Intent(MainActivity.this,MainActivity2.class);
+            Toast.makeText(MainActivity.this, "Covid 19 tracker is opening"
+                    , Toast.LENGTH_LONG).show();
+        startActivity(nextPage);
+
+        });
     }
 }
