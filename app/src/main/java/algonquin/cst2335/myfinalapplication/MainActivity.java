f370296c41edd1aba3b4fc0a0a4391332427b3b9
Darshan Singh <sing1489@algonquincollege.com>
2021-12-07 12:31:41 -0500

    NextPage Intent done (OW)

--- app/src/main/java/algonquin/cst2335/myfinalapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myfinalapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package algonquin.cst2335.myfinalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.owlbot);
+        btn.setOnClickListener(clk -> {
+
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+
+            startActivity(nextPage);
+        });
+
     }
+
 }
